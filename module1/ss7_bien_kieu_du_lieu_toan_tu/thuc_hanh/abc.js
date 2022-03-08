452b72688eb4963ed72d11563204b9ca1180e4f2
unknown <nguyennhuhoa2303@gmail.com>
2022-03-08 18:10:47 +0700

    em nop bai ngày 8/3

---
+++ module1/ss7_bien_kieu_du_lieu_toan_tu/thuc_hanh/abc.js
@@ -0,0 +1,15 @@
+var inputWidth;
+var inputHeight;
+ inputWidth= prompt("Enter the width");
+ inputHeight= prompt("Enter the Heigh");
+
+//  var width = parseInt(inputWidth);
+// var height = parseInt(inputHeight);
+
+var area = inputWidth * inputHeight;
+document.write("the area is: " + area);
+
+
+
+
+
