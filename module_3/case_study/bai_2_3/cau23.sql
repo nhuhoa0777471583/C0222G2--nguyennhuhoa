865613f8aa04726a36fda34c3e73a9eb6091b37c
unknown <nguyennhuhoa2303@gmail.com>
2022-05-14 15:36:13 +0700

    em nop bai

---
+++ module_3/case_study/bai_2_3/cau23.sql
@@ -0,0 +1,20 @@
+create database case_study_2_3;
+use case_study_2_3;
+create table cau_2(
+ma_nhan_vien int,
+ho_ten varchar (100)
+);
+insert into cau_2 value
+(3,'Hồ Thị Yến'),
+(9,'Tòng Hoang');
+select *from cau_2;
+
+use case_study_2_3;
+create table cau_3(
+ma_khach_hang int,
+ho_ten varchar(100));
+insert into cau_3 value
+(2,'Phạm Xuân Diệu'),
+(4,'Dương Văn Quan'),
+(10,'Nguyễn Tâm Đắc');
+select*from cau_3;
