134e01c12398e6276dc7fcb3ae862925aefa3831
unknown <nguyennhuhoa2303@gmail.com>
2022-05-30 17:09:28 +0700

    em nop bai

--- module_3/ss10_jsp_jstl/calculator/src/main/java/servlet/CalculatorServlet.java
+++ module_3/ss10_jsp_jstl/calculator/src/main/java/servlet/CalculatorServlet.java
@@ -8,29 +8,23 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "servlet.CalculatorServlet", value = "/calculator")
+@WebServlet(name = "servlet.CalculatorServlet", urlPatterns = "/calculator")
 public class CalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Double first = Double.parseDouble(request.getParameter("firstOperand"));
-        Double second = Double.parseDouble(request.getParameter("sencondOperand"));
-        char operator = request.getParameter("operator").charAt(0);
+    Double first = Double.parseDouble(request.getParameter("firstOperand"));
+    Double second = Double.parseDouble(request.getParameter("sencondOperand"));
+    char operator =request.getParameter("operator").charAt(0);
         PrintWriter writer = response.getWriter();
-        Double result = Calculator.calculate(first, second, operator);
-
-
-
-
-        try {
+        writer.println("<html>");
+        writer.println("<h1>Result:</h1>");
+        try{
+            Double result = Calculator.calculate(first, second, operator);
             writer.println(first + " " + operator + " " + second + " = " + result);
-        } catch (Exception ex) {
+        }catch (Exception ex){
             writer.println("Error: " + ex.getMessage());
         }
-        request.setAttribute("first", first);
-        request.setAttribute("second", second);
-        request.setAttribute("result", result);
-        request.getRequestDispatcher("index.jsp").forward(request, response);
-        writer.println("<h2>Result: " + result + "</h2 >";
+        writer.println("</html>");
     }
 
     @Override
