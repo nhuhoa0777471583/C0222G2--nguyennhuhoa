e9be8f880ce6d9be7a7975fdaa035eda414a42ad
unknown <nguyennhuhoa2303@gmail.com>
2022-06-20 19:58:59 +0700

    em nop bai

--- module_4/ss3_thymeleaf/thuc_hanh/quan_lí_du_lieu_khach_hang/src/main/java/com/codegym/controller/CustomersController.java
+++ module_4/ss3_thymeleaf/thuc_hanh/quan_lí_du_lieu_khach_hang/src/main/java/com/codegym/controller/CustomersController.java
@@ -1,4 +1,76 @@
 package com.codegym.controller;
 
+
+import com.codegym.model.Customer;
+import com.codegym.service.CustomersService;
+import com.codegym.service.ICustomerService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customer")
 public class CustomersController {
+    private ICustomerService iCustomerService = new CustomersService();
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Customer> customerList = this.iCustomerService.findAll();
+        model.addAttribute("customerList", customerList);
+        return "index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("customers", new Customer());
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String save(Customer customer, RedirectAttributes redirectAttributes) {
+        customer.setId((int) (Math.random() * 1000));
+        iCustomerService.save(customer);
+        redirectAttributes.addFlashAttribute("message","thêm mới thành công");
+        return "redirect:/customer";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("customer", iCustomerService.findById(id));
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Customer customer) {
+        iCustomerService.update(customer.getId(), customer);
+        return "redirect:/customer";
+    }
+
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("customer", iCustomerService.findById(id));
+        return "/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Customer customer, RedirectAttributes redirect) {
+        iCustomerService.remove(customer.getId());
+        redirect.addFlashAttribute("message", "Removed customer successfully!");
+        return "redirect:/customer";
+    }
+
+    @GetMapping("/{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("customer", iCustomerService.findById(id));
+        return "/view";
+    }
 }
