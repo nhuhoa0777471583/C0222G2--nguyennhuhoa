ee305aefa617782f09c4dbc0b015133515c3e358
unknown <nguyennhuhoa2303@gmail.com>
2022-06-20 19:58:02 +0700

    em nop bai

---
+++ module_4/ss3_thymeleaf/bai_tap/quan_li_san_pham/src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,81 @@
+package com.codegym.controller;
+
+
+import com.codegym.model.Product;
+import com.codegym.service.IProductService;
+import com.codegym.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    private static IProductService iProductService = new ProductService();
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Product> products = this.iProductService.findAll();
+        model.addAttribute("products", products);
+        return "index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("products", new Product());
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product, RedirectAttributes redirectAttributes) {
+        product.setId((int) (Math.random() * 1000));
+        iProductService.save(product);
+        redirectAttributes.addFlashAttribute("message", "Create product successfully!");
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showEdit(@PathVariable Integer id, Model model) {
+        model.addAttribute("products", iProductService.findById(id));
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String editProduct(Product product, RedirectAttributes redirectAttributes) {
+        iProductService.update(product.getId(), product);
+        redirectAttributes.addFlashAttribute("message", "Update product successfully");
+        return "redirect:/product";
+    }
+
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("product", iProductService.findById(id));
+        return "delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Product product, RedirectAttributes redirect) {
+        iProductService.delete(product.getId());
+        redirect.addFlashAttribute("message", "Removed customer successfully!");
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/view")
+    public String displayListProduct(@PathVariable Integer id, Model model) {
+        model.addAttribute("product", iProductService.findById(id));
+        return "view";
+    }
+
+    @GetMapping("/{nameSearch}/search")
+    public String searchByName(@PathVariable String nameSearch, Model model) {
+        model.addAttribute("product", iProductService.searchByName(nameSearch));
+        return "redirect:/product";
+    }
+}
