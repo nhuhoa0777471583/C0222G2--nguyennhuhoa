bc3c67cd2bb427d9bd97f1e440ec74ac3d1fd329
unknown <nguyennhuhoa2303@gmail.com>
2022-06-22 11:56:24 +0700

    up

--- module_4/ss5_thao_tac_csdl_orm/bai_tap/quan_li_san_pham/src/main/java/com/codegym/service/ProductService.java
+++ module_4/ss5_thao_tac_csdl_orm/bai_tap/quan_li_san_pham/src/main/java/com/codegym/service/ProductService.java
@@ -1,57 +1,50 @@
 package com.codegym.service;
 
 import com.codegym.model.Product;
+import com.codegym.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class ProductService implements IProductService {
-    private static Map<Integer, Product> products = new HashMap<>();
 
-    static {
-        products.put(1, new Product(1, "banh", 10.000, "ngon", "Viet Nam"));
-        products.put(2, new Product(2, "keo", 5.000, "ngon", "Viet Nam"));
-        products.put(3, new Product(3, "sua", 15.000, "ngon", "Viet Nam"));
-        products.put(4, new Product(4, "coca", 15.000, "ngon", "Trung Quoc"));
-        products.put(5, new Product(5, "bia", 15.000, "ngon", "Viet Nam"));
-    }
+   @Autowired
+   private IProductRepository iProductRepository;
 
 
     @Override
     public List<Product> findAll() {
-        return new ArrayList<>(products.values());
+        return iProductRepository.findAll();
     }
 
     @Override
     public void save(Product product) {
-        products.put(product.getId(), product);
+      iProductRepository.save(product);
     }
 
     @Override
     public Product findById(Integer id) {
-        return products.get(id);
+        return iProductRepository.findById(id);
     }
 
     @Override
-    public void update(Integer id, Product product) {
-        products.put(id, product);
+    public void update( Product product) {
+    iProductRepository.update(product);
     }
 
     @Override
     public void delete(Integer id) {
-        products.remove(id);
+       iProductRepository.delete(id);
     }
 
     @Override
     public List<Product> searchByName(String name) {
-        List<Product> productList = new ArrayList<>();
-        for (Product pro : products.values()){
-            if(pro.getNameProduct().contains(name)){
-                productList.add(pro);
-            }
-        }
-        return productList;
+
+        return null;
     }
 }
